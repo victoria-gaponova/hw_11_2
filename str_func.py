9cf2439cbbf8046bfac71917a4b2da43590d485a
Victoria Gaponova <victoria.gaponava@gmail.com>
2023-06-05 12:31:41 +0300

    add func make_title_first_letter(line)

--- str_func.py
+++ str_func.py
@@ -1,2 +1,6 @@
 def change_letters_uppercase(word):
     return word.upper()
+
+def make_title_first_letter(line):
+    """возвращает заглавными первые буквы каждого слова в строке"""
+    return line.title()
