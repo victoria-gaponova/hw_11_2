cc883cc840d63234cd7e948a4909460e9e70d217
Victoria Gaponova <victoria.gaponava@gmail.com>
2023-06-05 12:41:06 +0300

    add docstring func change_letters_uppercase

--- str_func.py
+++ str_func.py
@@ -1,5 +1,7 @@
 def change_letters_uppercase(word):
+    """возвращает слово заглавными буквами"""
     return word.upper()
+
 
 def make_title_first_letter(line):
     """возвращает заглавными первые буквы каждого слова в строке"""
